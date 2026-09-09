737923bbff5755ff1892d2cebc21765d660c85b1
Mayank singh <mayank.singh.gaherwar@gmail.com>
2026-09-09 13:21:25 +0530

    Time: 1 ms (99.97%), Space: 42.3 MB (90.41%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int reverse(int x) {
+        int reversed = 0;
+
+        while(x!=0){
+            int k = x%10;
+            x = x/10;
+            if(reversed>Integer.MAX_VALUE/10 || reversed<Integer.MIN_VALUE/10){
+            return 0;
+            }
+            reversed = reversed*10 + k;
+        }
+
+        
+
+        return reversed;
+    }
+}
